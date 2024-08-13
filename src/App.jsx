7c96a02b11dc2854ee2c07703240528d08ae09d1
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-08-13 16:03:11 +0300

    initial design no keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 // import { useState } from 'react';
 import './App.css';
+import hangman from './components/img/hangman.png';
 
 function App() {
   
   return (
     <>
-      <h1>GitHub Deploy</h1>
+    <div className='mainContainer'>
+      <h1>Guess a word</h1>
+      <div className="scoreContainer">
+        <div className="win">
+          <p>Wins: </p>
+          <span>0</span>
+        </div>
+        <div className="lose">
+          <p>Loses: </p>
+          <span>0</span>
+        </div>
+      </div>
+      <img src={hangman} alt="" />
+      <p>Result</p>
+      <div className='keyboardContainer'></div>
+    </div>
     </>
   )
 }
