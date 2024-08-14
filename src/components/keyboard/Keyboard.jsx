96d81300ac552782461af5c30d05e7fa6de0b836
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-08-14 08:36:13 +0300

    handleKeyPress + keyboard useEffect hook

--- src/components/keyboard/Keyboard.jsx
+++ src/components/keyboard/Keyboard.jsx
@@ -1,13 +1,31 @@
+import { useEffect } from 'react';
 import style from './Keyboard.module.css'
 
 export function Keyboard() {
     const keys = "QWERTYUIOPASDFGHJKLZXCVBNM".split('');
 
+    const handleKeyPress = (event) => {
+        const pressedKey = event.key.toUpperCase();
+        if (keys.includes(pressedKey)) {
+            console.log(`Key pressed: ${pressedKey}`);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('keyup', handleKeyPress);
+
+        return () => {
+            window.removeEventListener('keyup', handleKeyPress);
+        };
+    }, []);
+
     return (
         <div className={style.buttonContainer}>
-            {keys.map(key => {
-                return <button key={key} className={style.button}>{key}</button>
-            })}
+            {keys.map(key => (
+                <button key={key} className={style.button}
+                onClick={() => handleKeyPress({key})}>{key}
+                </button>
+            ))}
         </div>
     );
 }
--- src/components/wordGenerator/WordGenerator.jsx
+++ src/components/wordGenerator/WordGenerator.jsx
@@ -6,14 +6,13 @@ export function WordGenerator(){
     const [word] = useState(() =>{
         return wordList[Math.floor(Math.random() * wordList.length)]
     });
-
     const splitWord = word.toUpperCase().split('');
 
     return (
         <div className={style.wordDiv}>
             {splitWord.map((letter, index) => (
                 <span key={index}>{letter}</span>
-            ))}
+            ))}   
         </div>
     ); 
 }
